0ae82117845d19c6b72d4fe1a377c0abb50ef2e9
Nhut Do <mr.nhut@gmail.com>
2019-03-24 22:54:35 +0200

    Add secure to API.
    * API calls now requires basic authentication header. Username and password is configurable in the application.properties-file.

---
+++ src/main/java/fi/donhut/simplemonitorserver/config/SecurityConfig.java
@@ -0,0 +1,67 @@
+/*
+ * Copyright since 2019 Nhut Do <mr.nhut@gmail.com>
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *  http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package fi.donhut.simplemonitorserver.config;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
+import org.springframework.security.config.annotation.method.configuration.EnableGlobalMethodSecurity;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
+
+/**
+ * Security configurations.
+ *
+ * @author Nhut Do (mr.nhut@gmail.com)
+ */
+@Configuration
+@EnableGlobalMethodSecurity(prePostEnabled = true)
+@EnableWebSecurity
+public class SecurityConfig extends WebSecurityConfigurerAdapter {
+
+    @Autowired
+    private PasswordEncoder passwordEncoder;
+
+    @Value("${app.security.api.username}")
+    private String allowUsername;
+
+    @Value("${app.security.api.password}")
+    private String allowUserPassword;
+
+    @Override
+    protected void configure(AuthenticationManagerBuilder managerBuilder) throws Exception{
+        managerBuilder.inMemoryAuthentication()
+                .withUser(allowUsername).password(passwordEncoder.encode(allowUserPassword))
+                .roles(UserRole.API.toString());
+    }
+
+    @Override
+    protected void configure(final HttpSecurity httpSecurity) throws Exception {
+        httpSecurity.httpBasic().and().authorizeRequests()
+                .antMatchers("/api/**").hasRole(UserRole.API.toString())
+                .and().csrf().disable();
+    }
+
+    @Bean
+    public PasswordEncoder passwordEncoder(){
+        return new BCryptPasswordEncoder();
+    }
+}
